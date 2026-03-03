29349966fdc6b931a201f5117aed1517bff6d5ff
unknown <maraqamohamad@outlook.sa>
2026-03-03 19:32:24 +0200

    asd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -283,7 +283,7 @@
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M9 18l6-6-6-6"/>
             </svg>
-            العودة
+            الرئيسية
         </button>
 
         <button class="nav-arrow right-arrow" onclick="flipToPrev()" id="prev-btn">
