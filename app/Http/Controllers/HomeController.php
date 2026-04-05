078b9f041b6f330ef97860de8b065038f34494c1
DESKTOP-SFN4QMD\Mohamad Maraqa <maraqamohamad@outlook.sa>
2026-04-05 19:34:40 +0200

    asd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,10 +10,11 @@ class HomeController extends Controller
     public function index()
     {
         $categories = Category::with(['media', 'menuPages' => function($q) {
-                $q->where('is_active', true)->orderBy('sort_order');
+                $q->where('is_active', true)->orderBy('sort_order')->orderBy('id', 'asc');
             }, 'menuPages.media'])
             ->where('is_active', true)
             ->orderBy('sort_order')
+            ->orderBy('id', 'asc')
             ->get();
 
         $setting = \App\Models\Setting::first();
